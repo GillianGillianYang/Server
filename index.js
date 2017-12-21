b60a7f1b7fb46c4aa8373c9121c177c4af3d651c
GillianY <jjjgrace01@gmail.com>
2017-12-21 19:11:43 +0800

    server set up

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Mian Starting point
+// main starting point of the application
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const http = require('http');
+
+//App setup
+const app = express();
+
+// server setup
+const PORT = process.env.PORT || 3090;
+
+const server = http.createServer(app);
+server.listen(PORT,()=>{ 
+        console.log(`server has start up on port :${PORT}`);
+    });
+
+// app.listen(PORT,()=>{
+//     console.log(`server has start up on port :${PORT}`);
+// });    
